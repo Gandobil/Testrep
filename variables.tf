2c456fa8875b162fc191a9f1d63b7df66a638a7f
Gandobil <116957972+Gandobil@users.noreply.github.com>
2023-09-28 16:10:44 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable ami {
+        default = "ami-08a52ddb321b32a8c"
+}
+
+variable instance_type {
+        default = "t2.micro"
+}
+
+
+variable vpc_security_group_ids {
+        default = ["sg-0b07f33eb9e239395"]
+}
+
+
+variable name {}
+
+variable owner {}
+
+variable product {}
